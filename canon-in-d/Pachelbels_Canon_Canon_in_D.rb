de947cdd9e190cc59fc6d306075650a73f785756
Stanley Ndagi <ndagis@gmail.com>
2024-07-21 06:11:20 +0300

    add .editorconfig, modify melody_line

--- canon-in-d/Pachelbels_Canon_Canon_in_D.rb
+++ canon-in-d/Pachelbels_Canon_Canon_in_D.rb
@@ -12,6 +12,18 @@ bass_line = [
   [:a2, 2, 2]
 ]
 
+# Define the melody for "Canon in D"
+melody_line = [
+  [:fs5, 2, 2],
+  [:e5, 2, 2],
+  [:d5, 2, 2],
+  [:cs5, 2, 2],
+  [:b4, 2, 2],
+  [:a4, 2, 2],
+  [:b4, 2, 2],
+  [:cs5, 2, 2]
+]
+
 # Function to play the sequence with release
 define :play_sequence do |sequence|
   sequence.each do |note, duration, release|
@@ -25,13 +37,7 @@ live_loop :bass do
   play_sequence(bass_line)
 end
 
-# Define the melody for "Canon in D"
-melody_line = [:fs5, :e5, :d5, :cs5, :b4, :a4, :b4, :cs5]
-
 # Play the melody
 live_loop :melody do
-  melody_line.each do |note|
-    play note, release: 2
-    sleep 2
-  end
+  play_sequence(melody_line)
 end
