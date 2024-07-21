6955cbcdf49129cab14307cb13ffe2ac562df7c2
Stanley Ndagi <ndagis@gmail.com>
2024-07-21 05:56:19 +0300

    base line modified - canon in D

--- canon-in-d/Pachelbels_Canon_Canon_in_D.rb
+++ canon-in-d/Pachelbels_Canon_Canon_in_D.rb
@@ -1,10 +1,26 @@
-# Define the bass line for "Canon in D"
-bass_line = [:d2, :a2, :b2, :fs2, :g2, :d2, :g2, :a2]
+use_bpm 60  # Set the beats per minute
+
+# Define the bass line for "Canon in D" matching a 4/4 time signature with release
+bass_line = [
+  [:d3, 2, 2],
+  [:a2, 2, 2],
+  [:b2, 2, 2],
+  [:fs2, 2, 2],
+  [:g2, 2, 2],
+  [:d2, 2, 2],
+  [:g2, 2, 2],
+  [:a2, 2, 2]
+]
+
+# Function to play the sequence with release
+define :play_sequence do |sequence|
+  sequence.each do |note, duration, release|
+    play note, release: release
+    sleep duration
+  end
+end
 
 # Play the bass line
 live_loop :bass do
-  bass_line.each do |note|
-    play note, release: 1.5
-    sleep 1.5
-  end
+  play_sequence(bass_line)
 end
